24cf0e8779e532a5c3b57b9e8fb28eb058d926dd
Alicia Kirkland <akirkland@lclark.edu>
2016-04-27 14:47:08 -0700

    finishing project
    done (maybe)

--- final/project.py
+++ final/project.py
@@ -5,7 +5,11 @@ Created on Wed Apr 20 12:06:38 2016
 @author: akirkland, mgarfias
 """
 import csv 
+import math
 from matplotlib import pyplot as plt
+
+def mean(list):
+	return sum(list)/float(len(list))
 
 def read_data():
     with open("/Users/akirkland/Programming/datascience/final/timesData.csv", 'r', encoding='utf-8', errors="ignore") as file:
@@ -22,9 +26,7 @@ def read_data():
     location = [country['country'] for country in top]
       
     
-#   which country has most top ranking universities 
-#   works!
-#   TO DO: make a bar graph - DONE 
+#   dictionary of countries and top university count
     location_tracker = {}
     for place in location:
         if place in location_tracker:
@@ -57,14 +59,49 @@ def read_data():
     plt.xlabel("Country")
     plt.ylabel("Number of Top Universities")
     plt.title("Location of Top 200 Universities")
-#   problem: x labels overlap
     plt.xticks([i + 0.5 for i, _ in enumerate(countries)], countries)
     plt.show()
 
-#   female to male ration for top ranking universities
-#   works!
-#   TODO: make a line graph: rank vs. ration - done
+#   make a graph that maps countries to percentage of international students
+    cleaned = []
+    for school in top:
+        if school['international_students'] != '':
+            cleaned.append(school)
+    
+#    intl = [float(perc['international_students'].replace('%','')) for perc in cleaned]
 
+#    print(intl)
+
+    international = {}      
+    
+#   get average of international students in each country in the top 200
+    for key in location_tracker:
+        international[key] = 0;
+        values = []
+        for school in cleaned:
+            if school['country'] == key:
+                values.append(float(school['international_students'].replace('%','')))
+        international[key]=(mean(values))
+    
+    avg = []
+    countries = []
+    for key in international:
+        avg.append(international[key])
+        countries.append(lookup[key])
+        
+    xs = [i + 0.1 for i, _ in enumerate(countries)]
+        
+    plt.figure(figsize=(20,10))
+    plt.bar(xs, avg)
+    plt.xlabel("Country")
+    plt.ylabel("Average Amount of International Students (%)")
+    plt.title("Average Percentage of International Students at Top Universities in Various Countries")
+    plt.xticks([i + 0.5 for i, _ in enumerate(countries)], countries)
+    plt.show()           
+        
+
+
+#   female to male ration for top ranking universities
     ratio = []
     rank = []    
     
@@ -80,7 +117,6 @@ def read_data():
     plt.ylabel('Percentage of Female Students')
     plt.show()
 
-#   TODO: make a bar graph that groups together by 10s
     bar = {
         10 : 0,
         20: 0,
@@ -99,26 +135,21 @@ def read_data():
             if num <= key and num > (key-10):
                 bar[key] += 1
     
-#    print(bar)
     values = []
     for i in range(10, 101, 10):
         values.append(bar[i])
     
     labels = ['0-10', '10-20', '20-30', '30-40', '40-50', '50-60', '60-70', '70-80', '80-90', '90-100']    
-#    print(values)
     
     xs = [i + 0.1 for i, _ in enumerate(labels)]
         
     plt.bar(xs, values)
     plt.xlabel("Percentage of Females in Student Body")
     plt.ylabel("Number of Universities in Range")
-#    plt.title("Percentage")
-#   problem: x labels overlap
     plt.xticks([i + 0.5 for i, _ in enumerate(labels)], labels)
     plt.show()
 
 #   maps rank to income 
-#   TODO: line graph: rank vs income
     cleaned = []
     for school in top:
         if school['income'] != '-':
@@ -133,7 +164,6 @@ def read_data():
     plt.ylabel('Income (millions)')
     plt.show()
     
-#    print(incomes)
 
 #   maps rank to number of students
     cleaned = []
@@ -144,8 +174,7 @@ def read_data():
             
     
     rank = [rank['world_rank'] for rank in cleaned]
-    size = [int(population['num_students'].replace(',','')) for population in cleaned]
-    
+    size = [int(population['num_students'].replace(',','')) for population in cleaned] 
     
     labels = ['Small (0-5000)', 'Medium (5000-15000)', 'Large (15000+)']
     values = []
@@ -179,9 +208,13 @@ def read_data():
     plt.bar(xs, values)
     plt.xlabel("Size of Universities")
     plt.ylabel("Number of Universities in Range")
-#    plt.title("Percentage")
-#   problem: x labels overlap
     plt.xticks([i + 0.5 for i, _ in enumerate(labels)], labels)
+    plt.show()
+    
+    plt.figure()
+    plt.boxplot(size, 0, '')
+    plt.title('Distribution Display for Student Body Size')
+    plt.ylabel('Number of Students')
     plt.show()
             
     
@@ -201,11 +234,13 @@ def read_data():
     plt.xlabel('Student Body Size')
     plt.ylabel('Student-Staff Ratio')
     plt.show()
+    
+#   box plot of student staff ratio
+    plt.figure()
+    plt.boxplot(ratio, 0, '')
+    plt.title('Distribution Display for Student-Staff Ratio')
+    plt.ylabel('Students Per Faculty Member')
+    plt.show()
 
-#    print(rank)
-#    print(population)
-#            
-            
-   
         
 read_data()
